392b79a51fd910463832fd6a390350eee0770a38
moh9001 <mo9001@gmail.com>
2025-05-17 23:23:55 +0300

    Solved all tasks

--- python-data_structures/12-switch.py
+++ python-data_structures/12-switch.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 a = 89
 b = 10
-# YOUR CODE GOES HERE. DO NOT REMOVE THIS LINE
-a, b = b, a
+a, b = b, a # YOUR CODE GOES HERE. DO NOT REMOVE THIS LINE 
 print("a={} - b={}".format(a, b))
