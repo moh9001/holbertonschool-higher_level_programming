1c84d6b3f5f52e506278ddf174077c93be8d27e9
moh9001 <mo9001@gmail.com>
2025-05-17 21:58:04 +0300

    Added __name__ == '__main__' guards to all executable scripts

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 import sys
 
-argc = len(sys.argv) - 1  # number of actual arguments (excluding program name)
+if __name__ == "__main__":
+    argc = len(sys.argv) - 1
 
-if argc == 0:
-    print("0 arguments.")
-elif argc == 1:
-    print("1 argument:")
-else:
-    print("{} arguments:".format(argc))
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(argc))
 
-for i in range(1, len(sys.argv)):
-    print("{}: {}".format(i, sys.argv[i]))
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i]))
--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 import sys
 
-total = 0  # start sum at 0
-
-for i in range(1, len(sys.argv)):
-    total += int(sys.argv[i])  # convert argument to integer and add
-
-print("{}".format(total))
+if __name__ == "__main__":
+    total = 0
+    for i in range(1, len(sys.argv)):
+        total += int(sys.argv[i])
+    print("{}".format(total))
